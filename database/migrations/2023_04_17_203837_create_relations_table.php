3779391beb6186f57a19aa846d9e36aefe5b77c6
Muhammad Hannan <syedmhannan@gmail.com>
2023-04-17 15:41:01 -0700

    initial commit

---
+++ database/migrations/2023_04_17_203837_create_relations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('relations', function (Blueprint $table) {
+            $table->id();
+            // $table->unsignedBigInteger('careTakerId')->nullable();
+            // $table->foreign('careTakerId')->references('userId')->on('caretakers');
+            // $table->unsignedBigInteger('patientId')->nullable();
+            // $table->foreign('patientId')->references('userId')->on('patients');
+            $table->string('relationName');
+            $table->integer('otp');
+            $table->string('status');
+            $table->string('careTakerName');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('relations');
+    }
+};
